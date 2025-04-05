033677a181670894db02b1ae65cb73893e035dbc
mhmd-aashik <aashikdevelop@gmail.com>
2025-04-05 16:49:11 +0530

    ESlint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to DevFlow</h1>
+    <h1 className="text-3xl font-black text-violet-700">Welcome to DevFlow</h1>
   </div>
 );
 
