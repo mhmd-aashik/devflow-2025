88af7e098b302317fd29aebb203b965c13158242
mhmd-aashik <aashikdevelop@gmail.com>
2025-04-07 00:33:57 +0530

    Mobile Navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -11,19 +9,6 @@ const Home = async () => {
   return (
     <div>
       <h1 className="h1-bold font-space_grotesk">Welcome to DevFlow</h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({
-            redirectTo: ROUTES.SIGN_IN,
-          });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </div>
   );
 };
