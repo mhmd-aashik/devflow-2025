c9cb8f211727dd656242a51e5d785d4fbf5495be
mhmd-aashik <aashikdevelop@gmail.com>
2025-04-11 23:42:43 +0530

    Home Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,35 @@
+import Link from "next/link";
 import React from "react";
 
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
   console.log(session);
 
   return (
-    <div>
-      <h1 className="h1-bold font-space_grotesk">Welcome to DevFlow</h1>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Welcome to DevFlow</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Searchbar</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question card 1</p>
+        <p>Question card 1</p>
+        <p>Question card 1</p>
+        <p>Question card 1</p>
+      </div>
+    </>
   );
 };
 
--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,7 @@ const ROUTES = {
   SIGN_UP: "/sign-up",
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/routes/${id}`,
+  ASK_QUESTION: "/ask-question",
 };
 
 export default ROUTES;
